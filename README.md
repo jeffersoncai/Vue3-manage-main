fff2a0d6f87de8a5ad37d1df7ce46921b32efff0
jefferson44 <1015796337@qq.com>
2024-02-09 14:40:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,128 @@
+### 项目介绍
+
+本项目基于 Vue3 + TypeScript + Vite + Element-plus + Pinna + Echarts 实现的通用后台管理系统，可以作为管理系统模板使用，功能强大支持主题切换、JSON 表格、页面缓存、常用指令（防抖、节流）等。
+
+### 在线预览 👀
+
+- http://vue.aichat.ren/
+
+### 代码仓库
+
+给个 start 呗
+
+- GitHub：https://github.com/tangzihan-git/Vue3-manage
+
+### 主要功能介绍
+
+- 使用 Vue3 + TypeScript 开发
+- 采用 Vite 作为构建工具配置了 TSX、跨域、Gzip、去除 console 等
+- 对 Axios 进行二次封装，实现了请求拦截、token 无感刷新、取消重复请求等
+- 状态管理采用 Pinia，支持持久化配置
+- 支持主题配置，如布局切换、颜色切换、全局管理组件大小
+- 支持 i18n 国际化
+- VueRouter 方面有路由拦截、权限相关、动态路由、路由懒加载
+- 内置常用指令权限指令、防抖指令、节流指令、复制指令
+- 支持代码规范（不需要可关闭）采用 ESLint、Stylelint 校验
+- 使用 husky、lint-staged、commitlint、czg、cz-git 规范提交信息
+
+### 如何使用
+
+**第一步**
+
+```text
+git clone https://github.com/tangzihan-git/Vue3-manage.git
+```
+
+**第二步**
+
+```text
+cd Vue3-manage
+npm i
+```
+
+**第三步**
+
+```text
+npm run dev
+```
+
+### 系统环境说明
+
+系统支持开发、测试、生产三个环境，对应的环境变量在项目根目录
+.env.development 开发环境
+.env.production 生产环境
+.env.test 测试环境
+
+### 代码规范
+
+```text
+# eslint 检测代码
+npm run lint:eslint
+
+# prettier 格式化代码
+npm run lint:prettier
+
+# stylelint 格式化样式
+npm run lint:stylelint
+```
+
+### 项目截图
+
+### 项目架构
+
+```text
+Vue3-manage
+├─ .husky                 # husky 配置文件
+├─ .vscode                # VSCode 推荐配置
+├─ build                  # Vite 配置项
+├─ public                 # 静态资源文件（该文件夹不会被打包）
+├─ src
+│  ├─ api                 # API 接口管理
+│  ├─ assets              # 静态资源文件
+│  ├─ components          # 全局组件
+│  ├─ config              # 全局配置项
+│  ├─ directives          # 全局指令文件
+│  ├─ enums               # 项目常用枚举
+│  ├─ hooks               # 常用 Hooks 封装
+│  ├─ languages           # 语言国际化 i18n
+│  ├─ layouts             # 框架布局模块
+│  ├─ routers             # 路由管理
+│  ├─ stores              # pinia store
+│  ├─ styles              # 全局样式文件
+│  ├─ typings             # 全局 ts 声明
+│  ├─ utils               # 常用工具库
+│  ├─ views               # 项目所有页面
+│  ├─ App.vue             # 项目主组件
+│  ├─ main.ts             # 项目入口文件
+│  └─ vite-env.d.ts       # 指定 ts 识别 vue
+├─ .editorconfig          # 统一不同编辑器的编码风格
+├─ .env                   # vite 常用配置
+├─ .env.development       # 开发环境配置
+├─ .env.production        # 生产环境配置
+├─ .env.test              # 测试环境配置
+├─ .eslintignore          # 忽略 Eslint 校验
+├─ .eslintrc.cjs          # Eslint 校验配置文件
+├─ .gitignore             # 忽略 git 提交
+├─ .prettierignore        # 忽略 Prettier 格式化
+├─ .prettierrc.cjs        # Prettier 格式化配置
+├─ .stylelintignore       # 忽略 stylelint 格式化
+├─ .stylelintrc.cjs       # stylelint 样式格式化配置
+├─ CHANGELOG.md           # 项目更新日志
+├─ commitlint.config.cjs  # git 提交规范配置
+├─ index.html             # 入口 html
+├─ LICENSE                # 开源协议文件
+├─ lint-staged.config.cjs # lint-staged 配置文件
+├─ package-lock.json      # 依赖包包版本锁
+├─ package.json           # 依赖包管理
+├─ postcss.config.cjs     # postcss 配置
+├─ README.md              # README 介绍
+├─ tsconfig.json          # typescript 全局配置
+└─ vite.config.ts         # vite 全局配置文件
+```
+
+### 后端接口
+
+接口采用 mock 数据
+
+- FastMock： https://www.fastmock.site
+- EasyMock：https://mock.mengxuegu.com
---
+++ src/views/form/proForm/index.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="card content-box">
+    <el-alert
+      title="通过 component :is 组件属性 && v-bind 属性透传，可以将 template 中的 html 代码全部改变为 columns 配置项，具体配置请看代码。"
+      type="warning"
+      :closable="false"
+    />
+    <component :is="'el-form'" v-bind="options.form" ref="proFormRef" :model="model">
+      <template v-for="item in options.columns" :key="item.prop">
+        <component :is="'el-form-item'" v-bind="item.formItem">
+          <component :is="`el-${item.attrs.typeName}`" v-bind="item.attrs" v-model="model[item.formItem.prop]" />
+        </component>
+      </template>
+      <el-form-item>
+        <slot name="operation"></slot>
+      </el-form-item>
+    </component>
+  </div>
+</template>
+
+<script setup lang="ts" name="proForm">
+import { ref } from "vue";
+
+let model = ref<any>({});
+
+const options = ref({
+  // 表单整体配置项
+  form: {
+    inline: false,
+    labelPosition: "right",
+    labelWidth: "80px",
+    size: "default",
+    disabled: false,
+    labelSuffix: " :"
+  },
+  // 表单列配置项 (formItem 代表 item 配置项，attrs 代表 输入、选择框 配置项)
+  columns: [
+    {
+      formItem: {
+        label: "设计煤收到基碳",
+        prop: "tan",
+        labelWidth: "200px",
+        required: true
+      },
+      attrs: {
+        typeName: "input",
+        clearable: true,
+        placeholder: "请输入",
+        style: "width:500px"
+        // disabled: true
+      }
+    },
+    {
+      formItem: {
+        label: "设计煤收到基氢",
+        prop: "qing",
+        labelWidth: "200px",
+        required: true
+      },
+      attrs: {
+        typeName: "input",
+        placeholder: "请输入",
+        clearable: true,
+        style: "width:500px"
+      }
+    },
+    {
+      formItem: {
+        label: "设计煤收到基氧",
+        prop: "yang",
+        labelWidth: "200px",
+        required: true
+      },
+      attrs: {
+        typeName: "input",
+        placeholder: "请输入",
+        clearable: true,
+        style: "width:500px"
+      }
+    },
+    {
+      formItem: {
+        label: "设计煤收到基氮",
+        prop: "dan",
+        labelWidth: "200px",
+        required: true
+      },
+      attrs: {
+        typeName: "input",
+        clearable: true,
+        placeholder: "请输入",
+        style: "width:500px"
+        // disabled: true
+      }
+    },
+    {
+      formItem: {
+        label: "设计煤收到基硫",
+        prop: "liu",
+        labelWidth: "200px",
+        required: true
+      },
+      attrs: {
+        typeName: "input",
+        clearable: true,
+        placeholder: "请输入",
+        style: "width:500px"
+        // disabled: true
+      }
+    },
+    {
+      formItem: {
+        label: "设计煤收到基灰分",
+        prop: "huifen",
+        labelWidth: "200px",
+        required: true
+      },
+      attrs: {
+        typeName: "input",
+        clearable: true,
+        placeholder: "请输入",
+        style: "width:500px"
+        // disabled: true
+      }
+    },
+    {
+      formItem: {
+        label: "全水分",
+        prop: "shuifen",
+        labelWidth: "200px",
+        required: true
+      },
+      attrs: {
+        typeName: "input",
+        clearable: true,
+        placeholder: "请输入",
+        style: "width:500px"
+        // disabled: true
+      }
+    },
+    {
+      formItem: {
+        label: "设计煤收到基低位发热量",
+        prop: "liu",
+        labelWidth: "200px",
+        required: true
+      },
+      attrs: {
+        typeName: "input",
+        clearable: true,
+        placeholder: "请输入",
+        style: "width:500px"
+        // disabled: true
+      }
+    }
+  ]
+});
+</script>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
